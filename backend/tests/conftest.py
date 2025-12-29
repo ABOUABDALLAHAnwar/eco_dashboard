e099b07e2dc76bafa35b8cda1a0f42464cb1cac7
Anwar Abouabdallah <maabouabdallah@gmail.com>
2025-12-29 15:18:31 +0100

    mock quartier coordinate

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,6 +1,8 @@
+from unittest.mock import patch
+
 import mongomock
 import pytest
-from unittest.mock import patch
+
 
 @pytest.fixture
 def mockdb():
@@ -32,13 +34,15 @@ def user_profile_infos_collection(mockdb):
     user_profile_infos_collection = mockdb["user_profile"]
     return user_profile_infos_collection
 
+
 @pytest.fixture(autouse=True)
 def mock_get_coords():
     from backend.compute_tools import quartier_cordonnes
+
     with patch.object(quartier_cordonnes, "get_coords") as mock:
         # Retourne les coordonnées que tu as testées en local
         mock.side_effect = lambda address: {
             "Aeroport Bordeaux": (44.8292748, -0.7125609),
-            "7 rue Réné Bonnac, Cenon": (44.8570121, -0.5306309)
+            "7 rue Réné Bonnac, Cenon": (44.8570121, -0.5306309),
         }.get(address, (0.0, 0.0))
         yield mock
