a334c3a03f4e3b85db7ff347ac8b53eb27530d22
Anwar Abouabdallah <maabouabdallah@gmail.com>
2026-01-02 23:25:09 +0100

    crrection of the test

--- backend/tests/unit/test_variables.py
+++ backend/tests/unit/test_variables.py
@@ -1,3 +1,4 @@
+import backend.scripts.emissions_factors as emissions_factors
 import backend.scripts.variables as cfg  # remplace par le nom réel du fichier
 
 
@@ -7,5 +8,5 @@ def test_configs_import():
     assert cfg.ACCESS_TOKEN_EXPIRE_MINUTES == 300
 
     # vérifier que les dictionnaires existent
-    assert "petite" in cfg.facteurs
-    assert "bus" in cfg.factors
+    assert "petite" in emissions_factors.facteurs
+    assert "bus" in emissions_factors.factors
